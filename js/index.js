18d7fd95ac54fed4276ed439e5d6eb72fe32c527
wbh1234 <18249214675@163.com>
2017-10-12 23:05:06 +0800

    My personal websit the background come on the first day the two day

---
+++ js/index.js
@@ -0,0 +1,68 @@
+/**
+ * Created by ASUS on 2017/10/12.
+ */
+var oBackground= document.getElementById("background");
+var oBackground_list = document.getElementById("background_list");
+var aLi = oBackground_list.getElementsByTagName("li");
+var oBackground_img = document.getElementById("background_img");
+var aImg = oBackground_img.getElementsByTagName("img");
+var oPrev = document.getElementById("prev");
+var oNext = document.getElementById("next");
+var nowIndex = 0;//全局变量，保存当前显示的图片的索引
+var timer;
+for(var i=0; i<aLi.length; i++){
+    aLi[i].index = i;//给每个li对象自定义属性，保存住当时的i的值
+    /*
+     相当于：
+     aLi[0].index = 0;
+     aLi[1].index = 1;
+     aLi[2].index = 2;
+     aLi[3].index = 3;*/
+    aLi[i].onmouseover = function(){
+        nowIndex = this.index;
+        changeImg();
+    };
+}
+
+oPrev.onclick = oNext.onclick = function(){
+    if(this === oPrev){
+        nowIndex--;
+        if(nowIndex == -1){
+            nowIndex = aLi.length - 1;//3
+        }
+    }else{
+        nowIndex++;
+        if(nowIndex == aLi.length){
+            nowIndex = 0;
+        }
+    }
+    changeImg();
+};
+
+oPrev.onselectstart = oNext.onselectstart = function(){
+    return false;
+};
+
+oBackground.onmouseover = function(){
+    clearInterval(timer);
+};
+oBackground.onmouseout = function(){
+    play();
+};
+
+play();
+
+function play(){
+    timer = setInterval(function(){
+        oNext.onclick();
+    }, 1500);
+}
+
+function changeImg(){
+    for(var i=0; i<aLi.length; i++){
+        aLi[i].className = "";
+        aImg[i].className = "";
+    }
+    aLi[nowIndex].className = "selected";
+    aImg[nowIndex].className = "selected";
+}
